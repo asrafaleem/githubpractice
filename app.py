316600e02c48759961db63fe1e27a9b68f3c9109
Asraf A <asrafaleem8@gmail.com>
2026-09-05 17:15:23 +0530

    Add login functionality

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ def welcome():
     print("Welcome to my application")
 
 
+def login(username, password):
+    if username == "admin" and password == "1234":
+        print("Login successful")
+    else:
+        print("Invalid username or password")
+
+
 welcome()
+login("admin", "1234")
